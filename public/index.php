4580bdefa1047a68bab994a4373e6e7b2bcdcb70
Jorge A Ramirez G <jramirezgranada@gmail.com>
2019-01-28 12:52:15 -0500

    cors errors

--- public/index.php
+++ public/index.php
@@ -8,7 +8,7 @@
  */
 header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Methods: GET, POST, PATCH, PUT, DELETE, OPTIONS');
-header('Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token');
+header('Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token, Accept');
 
 define('LARAVEL_START', microtime(true));
 
